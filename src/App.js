0e41d01b1219f30bcc197039c27a5b5b59967513
anandkchandran <anandkchandran@yahoo.com>
2020-04-25 02:09:15 +0530

    remaining files in src folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import CardList from "./component/cardlist/cardlist.component";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* class App extends Component {
+  constructor(){
+    super();
+    console.dir("constructor is called first");
+    this.state = {
+      monsters : []
+    }
+  }
 
-export default App;
+  componentDidMount(){
+    console.dir("didmount is called second");
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => this.setState({
+        monsters : json
+      }))
+  }
+
+  
+
+  render(){
+    return(
+      <div>
+        {this.state.monsters.map(monster => <div key={monster.id}> Hey! {monster.name}</div>)}
+        <CardList monsters={this.state.monsters}/>
+      </div>
+      
+    )
+  }
+
+} */
+
+export const App = (props) => {
+  return(
+    <CardList />
+  )
+};
